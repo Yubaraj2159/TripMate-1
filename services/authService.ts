d0a19c411324921893aab0bcf7f83b27abff5397
Yuvraj2159 <132037227+Yuvraj2159@users.noreply.github.com>
2025-12-11 20:20:22 +0545

    Services added

---
+++ services/authService.ts
@@ -0,0 +1,15 @@
+// services/authService.ts
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../config/firebaseConfig";
+
+export const registerUser = (email: string, password: string) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+export const loginUser = (email: string, password: string) =>
+  signInWithEmailAndPassword(auth, email, password);
+
+export const logoutUser = () => signOut(auth);
