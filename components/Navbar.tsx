c2ad9584a45e5f89be194113f00b1ccc182d6b0e
Yuvraj2159 <132037227+Yuvraj2159@users.noreply.github.com>
2025-12-21 21:08:32 +0545

    Add Navbar component implementation

---
+++ components/Navbar.tsx
@@ -0,0 +1,6 @@
+// this is the navbar component for the mobile app
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
